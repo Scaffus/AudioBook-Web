bdbcabb716381bce73b761d5bf998a923787267c
Scaffus <scaffus.scaffus@gmail.com>
2021-04-12 14:14:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
