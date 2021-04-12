f10c49ba8b70b3022c3c975996562da39ad98c1b
Scaffus <scaffus.scaffus@gmail.com>
2021-04-12 14:23:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
 from datetime import datetime
+import txt2audio
 
 
 app = Flask(__name__)
